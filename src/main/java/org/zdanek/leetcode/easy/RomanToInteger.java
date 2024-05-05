4768a650897450d56b2e2d75db2f719efa19f8f8
MaZdan6 <fazol66@gmail.com>
2024-05-05 13:26:26 +0200

    RomanToInteger easy

--- src/main/java/org/zdanek/leetcode/easy/RomanToInteger.java
+++ src/main/java/org/zdanek/leetcode/easy/RomanToInteger.java
@@ -31,38 +31,35 @@ public class RomanToInteger {
                 romanNext = romans.get(i + 1);
             }
 
-            //I jest przed  (V lub X) => (V lub X) - I
-            if (roman.equals("I")) {
-                if (romanNext.equals("V") || romanNext.equals("X")) {
-                    arabic = map.get(romanNext) - map.get("I");
-                    arabics.add(arabic);
-                    i++;
-                    continue;
-                }
+            switch (roman) {
+                case "I":
+                    if (romanNext.equals("V") || romanNext.equals("X")) {
+                        arabic = map.get(romanNext) - map.get("I");
+                        arabics.add(arabic);
+                        i++;
+                        continue;
+                    }
+                case "X":
+                    if (romanNext.equals("L") || romanNext.equals("C")) {
+                        arabic = map.get(romanNext) - map.get("X");
+                        arabics.add(arabic);
+                        i++;
+                        continue;
+                    }
+                case "C":
+                    //C jest przed  (D lub M) => (D lub M) - X
+                    if (romanNext.equals("D") || romanNext.equals("M")) {
+                        arabic = map.get(romanNext) - map.get("C");
+                        arabics.add(arabic);
+                        i++;
+                        continue;
+                    }
+                default:
+                    arabics.add(map.get(roman));
             }
-            //X jest przed  (L lub C) => (L lub C) - X
-            if (roman.equals("X")) {
-                if (romanNext.equals("L") || romanNext.equals("C")) {
-                    arabic = map.get(romanNext) - map.get("X");
-                    arabics.add(arabic);
-                    i++;
-                    continue;
-                }
-            }
-            //C jest przed  (D lub M) => (D lub M) - X
-            if (roman.equals("C")) {
-                if (romanNext.equals("D") || romanNext.equals("M")) {
-                    arabic = map.get(romanNext) - map.get("C");
-                    arabics.add(arabic);
-                    i++;
-                    continue;
-                }
-            }
-            arabics.add(map.get(roman));
         }
-
         //sumuj wszystkie liczby
-        int output=arabics.stream()
+        int output = arabics.stream()
                 .reduce(0, Integer::sum);
 
         return output;
