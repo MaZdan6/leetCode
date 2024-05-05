fab3ba9de96600c6ebe75453f1e2cf6d882b449c
MaZdan6 <fazol66@gmail.com>
2024-05-05 22:16:47 +0200

    RomanToInteger easy

--- src/main/java/org/zdanek/leetcode/easy/RomanToInteger.java
+++ src/main/java/org/zdanek/leetcode/easy/RomanToInteger.java
@@ -8,60 +8,54 @@ import java.util.List;
 public class RomanToInteger {
     public int romanToInt(String s) {
         // mapa znaków
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("I", 1);
-        map.put("V", 5);
-        map.put("X", 10);
-        map.put("L", 50);
-        map.put("C", 100);
-        map.put("D", 500);
-        map.put("M", 1000);
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
         //rozbij string na znaki
-        List<String> romans = Arrays.asList(s.split(""));
-        List<Integer> arabics = new ArrayList<>();
+        //List<String> romans = Arrays.asList(s.split(""));
+        char[] romans = s.toCharArray();
 
-        String roman;
-        String romanNext;
-        Integer arabic;
-        for (int i = 0; i < romans.size(); i++) {
-            roman = romans.get(i);
-            romanNext = "";
-            if (i + 1 < romans.size()) {
-                romanNext = romans.get(i + 1);
+        char roman;
+        char romanNext;
+        Integer arabic = 0;
+        for (int i = 0; i < romans.length; i++) {
+            roman = romans[i];
+            romanNext = ' ';
+            if (i + 1 < romans.length) {
+                romanNext = romans[i + 1];
             }
 
             switch (roman) {
-                case "I":
-                    if (romanNext.equals("V") || romanNext.equals("X")) {
-                        arabic = map.get(romanNext) - map.get("I");
-                        arabics.add(arabic);
+                case 'I':
+                    if (romanNext == 'V' || romanNext == 'X') {
+                        arabic += map.get(romanNext) - map.get('I');
                         i++;
                         continue;
                     }
-                case "X":
-                    if (romanNext.equals("L") || romanNext.equals("C")) {
-                        arabic = map.get(romanNext) - map.get("X");
-                        arabics.add(arabic);
+                case 'X':
+                    if (romanNext == 'L' || romanNext == 'C') {
+                        arabic += map.get(romanNext) - map.get('X');
                         i++;
                         continue;
                     }
-                case "C":
+                case 'C':
                     //C jest przed  (D lub M) => (D lub M) - X
-                    if (romanNext.equals("D") || romanNext.equals("M")) {
-                        arabic = map.get(romanNext) - map.get("C");
-                        arabics.add(arabic);
+                    if (romanNext == 'D' || romanNext == 'M') {
+                        arabic += map.get(romanNext) - map.get('C');
                         i++;
                         continue;
                     }
                 default:
-                    arabics.add(map.get(roman));
+                    arabic += map.get(roman);
             }
         }
-        //sumuj wszystkie liczby
-        int output = arabics.stream()
-                .reduce(0, Integer::sum);
 
-        return output;
+        return arabic;
     }
 }
