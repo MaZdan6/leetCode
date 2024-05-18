4d9978b592652b63dda144f6a5d685d671d4f478
MaZdan6 <fazol66@gmail.com>
2024-05-18 22:36:33 +0200

    35. Search Insert Position easy

--- src/main/java/org/zdanek/leetcode/easy/SearchInsertPosition.java
+++ src/main/java/org/zdanek/leetcode/easy/SearchInsertPosition.java
@@ -12,45 +12,24 @@ public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
 
         int a = 0;
-        int aPrev = 0;
         int b = nums.length - 1;
-        int bPrev = nums.length - 1;
-        int c;
         int d;
         int output = 0;
 
         while (a <= b) {
-            System.out.println(String.format("a=%s ,b=%s",a,b));
-            c = nums[a];
+            //System.out.println(String.format("a=%s ,b=%s", a, b));
             d = nums[b];
-            if (target == c) {
-                output = a;
-                break;
-            }else if (target < c) {
-                if(a-1>0){
-                    if(nums[a-1]<target){
-                        output=a;
-                    }else {
-                        output=a-1;
-                    }
-                }
-                break;
-            }else if(target>c)  {
-                output=a;
-            }else  {
-                a++;
-            }
             if (target == d) {
-                output=b;
+                output = b;
                 break;
-            }else if (target > d) {
-                output=b+1;
+            } else if (target > d) {
+                output = b + 1;
                 break;
-            }else {
+            } else {
                 b--;
             }
 
-            System.out.println("output: " + output);
+            //System.out.println("output: " + output);
         }
 
         return output;
